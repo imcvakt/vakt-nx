8c4eff321c2f67b489f04c8dcfbdce238417182b
Iorran <iorran.castro@vakt.io>
2020-07-26 22:46:59 +0100

    Change url to https

--- libs/logistics/data-access/src/lib/services/api.ts
+++ libs/logistics/data-access/src/lib/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://my-json-server.typicode.com/imcvakt/fake-api/'
+  baseURL: 'https://my-json-server.typicode.com/imcvakt/fake-api/'
   // baseURL: 'https://api.jsonbin.io/b/5f1cc9a9918061662848f0be/',
   // headers: {
   //   secret: '$2b$10$DMwTrGpCfLlgwKold7XOseXJS/dXXJY2cORaBhTe8x.tUZqk47x16'
