3458636495b901bc832b0bdbe5d23dc654a19805
Iorran <iorran.castro@vakt.io>
2020-08-04 12:47:29 +0100

    Initial Grid planner

--- libs/logistics/feature-planner/src/lib/trades-panel/trades-panel.tsx
+++ libs/logistics/feature-planner/src/lib/trades-panel/trades-panel.tsx
@@ -7,22 +7,58 @@ import styled from 'styled-components';
 export interface TradesPanelProps {}
 
 const StyledTitle = styled.div`
-  margin-bottom: 8px;
-  height: 22px;
   text-align: left;
-  font-size: 18px;
-  font-weight: bold;
+  font: Bold 18px/23px Helvetica Neue;
+  letter-spacing: 0px;
   color: #FFFFFF;
   opacity: 1;
+
+  margin-top: 8px;
+  margin-bottom: 16px;
 `;
 
 export const TradesPanel = (props: TradesPanelProps) => {
+  const headers = [
+    {item: 'Customer Trade', value: 'EL-94631-001'},
+    {item: 'Counterparty Trade Creator', value: 'Gunvor SA'},
+    {item: 'Product', value: 'Diesel'},
+    {item: 'Delivery Term', value: 'CIF'},
+    {item: 'Location', value: 'Antwerp'},
+    {item: 'Barge Name', value: '-'},
+    {item: 'Loading Date', value: '-'},
+    {item: 'Discharge Date', value: '-'},
+    {item: 'Inspector', value: '-'},
+    {item: 'Nomination Status', value: 'Pending Draft'},
+    {item: 'Timestamp', value: '-'}
+  ];
+
+  const openCommitments = {
+    headers: [
+      '',
+      'Counterparty Trade Creator',
+      'Quantity UoM',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      ''
+    ],
+    items: [
+      [ 'parcel', 'Gunvor SA', '4000 MMT', '', '', '', '', '', '', '', ''],
+      [ 'parcel', 'Gunvor SA', '4000 MMT', '', '', '', '', '', '', '', ''],
+      [ 'parcel', 'Gunvor SA', '4000 MMT', '', '', '', '', '', '', '', ''],
+      [ 'parcel', 'Gunvor SA', '4000 MMT', '', '', '', '', '', '', '', ''],
+    ]
+  };
   return (
     <>
       <StyledTitle>
         Trades
       </StyledTitle>
-      <NominationPlanner />
+      <NominationPlanner headers={headers} data={openCommitments} />
       <StyledTitle>
         Stocks
       </StyledTitle>
--- libs/logistics/ui/src/lib/nomination-planner/nomination-planner.tsx
+++ libs/logistics/ui/src/lib/nomination-planner/nomination-planner.tsx
@@ -3,17 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
-export interface NominationPlannerProps {}
-
-const StyledNominationPlanner = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 220px;
-  background: #212528 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  padding: 8px;
-`;
+export interface NominationPlannerProps {
+  headers: {item: string, value: string}[],
+  data: {
+    headers: string[],
+    items: string[][],
+  }
+}
 
 const ActionColumn = styled.div`
   display: flex;
@@ -22,153 +18,145 @@ const ActionColumn = styled.div`
   padding: 32px 16px;
 `;
 
+const Grid = styled.div<{rows: number, columns: number}>`
+  display: grid;
+  grid-template-columns: 0.2fr repeat(${p => p.columns}, 1fr) 0.2fr;
+  grid-template-rows: repeat(${p => p.rows}, auto);
+  grid-column-gap: 0px;
+  grid-row-gap: 8px;
+
+  background: #212528;
+  border-radius: 3px;
+  width: 100%;
+
+  padding: 8px;
+`;
+
+const Left = styled.div`
+  grid-area: 1 / 1 / 4 / 2;
+`;
+
+const Header = styled.div`
+  grid-area: 1 / 2 / 2 / 13;
+  border-bottom: 1px solid #424347;
+  margin-bottom: 16px;
+`;
+
+const Right = styled.div`
+  grid-area: 1 / 13 / 4 / 14;
+`;
+
+const Header2 = styled.div`
+  grid-area: 2 / 2 / 3 / 13;
+  background-color: #1A1A1A;
+`;
+
+const Items = styled.div<{rows: number}>`
+  grid-area: 3 / 2 / ${p => p.rows} / 13;
+`;
+
+const ColumnsFlex = styled.div`
+  display: flex;
+`;
+
 const ItemHead = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: flex-start;
+  justify-content: center;
+
+  height: 50px;
 
   > div:first-child {
-    text-align: left;
     font: Medium 13px Helvetica Neue;
-    letter-spacing: 0px;
     color: #399BF8;
-    opacity: 1;
   }
 
   > div {
-    text-align: left;
     font: Regular 13px/15px Helvetica Neue;
-    letter-spacing: 0px;
     color: #FFFFFF;
-    opacity: 1;
   }
 `;
 
 const ItemBody = styled.div`
-  text-align: left;
-  font: Medium 13px/16px Helvetica Neue;
-  letter-spacing: 0px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
   color: #6D9AC4;
-  opacity: 1;
+
+  border-radius: 3px 3px 0px 0px;
+  background-color: #1A1A1A;
+
+  height: 30px;
+`;
+
+const OpenCommitment = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #1A1A1A;
+  margin-bottom: 8px;
+  height: 40px;
 `;
 
 const Actions = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: space-evenly;
 `;
 
+export const NominationPlanner = ({ headers, data }: NominationPlannerProps) => {
 
-const ContentColumn = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-`;
-
-const DivTable = styled.div`
-  display: table;
-  width: 100%;
-`;
-
-const DivTableBody = styled.div`
-  display: table-row-group;
-`;
-
-const DivTableRow = styled.div`
-  display: table-row;
-`;
-
-const DivTableCell = styled.div`
-  border: 1px solid #999999;
-  display: table-cell;
-  padding: 3px 10px;
-`;
-
-export const NominationPlanner = (props: NominationPlannerProps) => {
   return (
-    <StyledNominationPlanner>
-      <ActionColumn>
-        <input type="checkbox" />
-      </ActionColumn>
-      <ContentColumn>
-        <DivTable>
-          <DivTableBody>
-            <DivTableRow>
-              <DivTableCell>&nbsp;</DivTableCell>
-              <DivTableCell>
-                <ItemHead>
-                  <div>Customer Trade</div>
-                  <div>EL-94631-001</div>
-                </ItemHead>
-              </DivTableCell>
-              <DivTableCell>
-                <ItemHead>
-                  <div>Counterparty Trade Creator</div>
-                  <div>Gunvor SA</div>
-                </ItemHead>
-              </DivTableCell>
-              <DivTableCell>
-                <ItemHead>
-                  <div>Counterparty Trade Creator</div>
-                  <div>Gunvor SA</div>
-                </ItemHead>
-              </DivTableCell>
-            </DivTableRow>
-            <DivTableRow>
-              <DivTableCell>
-                <Actions>
-                  <input type="checkbox" />
-                  <img width={16} height={16} src="assets/parcel.svg" alt="Opeen commitment type" />
-                </Actions>
-              </DivTableCell>
-              <DivTableCell>
-                <ItemBody>Counterparty Trade Creator</ItemBody>
-              </DivTableCell>
-              <DivTableCell>
-                <ItemBody>Quantity UoM</ItemBody>
-              </DivTableCell>
-              <DivTableCell>&nbsp;</DivTableCell>
-            </DivTableRow>
-          </DivTableBody>
-        </DivTable>
-        {/* <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>
-                <Item>
-                  <div>Customer Trade</div>
-                  <div>EL-94631-001</div>
-                </Item>
-              </th>
-              <th>
-                <Item>
-                  <div>Counterparty Trade Creator</div>
-                  <div>Gunvor SA</div>
-                </Item>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <Actions>
-                  <input type="checkbox" />
-                  <img width={16} height={16} src="assets/parcel.svg" alt="Opeen commitment type" />
-                </Actions>
-              </td>
-              <td>
-                <ItemBody>Counterparty Trade Creator</ItemBody>
-              </td>
-              <td>
-                <ItemBody>Quantity UoM</ItemBody>
-              </td>
-            </tr>
-          </tbody>
-        </table> */}
-      </ContentColumn>
-    </StyledNominationPlanner>
+    <Grid rows={(data.items[0].length + 2)} columns={headers.length}>
+      <Left>
+        <ActionColumn>
+          <input type="checkbox" />
+        </ActionColumn>
+      </Left>
+
+      <Header>
+        <ColumnsFlex>
+          {headers.map((header, i) => (
+            <ItemHead key={i}>
+              <div>{header.item}</div>
+              <div>{header.value}</div>
+            </ItemHead>
+          ))}
+        </ColumnsFlex>
+      </Header>
+
+      <Header2>
+        <ColumnsFlex>
+          {data?.headers.map((header, i) => (
+            <ItemBody key={i}>{header}</ItemBody>
+          ))}
+        </ColumnsFlex>
+      </Header2>
+
+      <Items rows={data.items[0].length}>
+        {data?.items[0].map((item, itemIndex) => (
+          <ColumnsFlex key={itemIndex}>
+            {data?.items[0].map((subItem, subItemIndex) => (
+              <OpenCommitment key={subItemIndex}>
+                {subItemIndex === 0 && (
+                  <Actions>
+                    <input type="checkbox" />
+                    <img width={16} height={16} src="assets/parcel.svg" alt="Open commitment type" />
+                  </Actions>
+                )}
+                {subItemIndex !== 0 && subItem}
+              </OpenCommitment>
+            ))}
+          </ColumnsFlex>
+        ))}
+      </Items>
+
+      <Right>{'<'}</Right>
+    </Grid>
   );
 };
 
