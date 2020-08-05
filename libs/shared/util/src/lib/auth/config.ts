e414f474550aac877655fd4ed1095f3902683f7d
Iorran <iorran.castro@vakt.io>
2020-08-06 00:28:59 +0100

    QA03 keycloack config

--- libs/shared/util/src/lib/auth/config.ts
+++ libs/shared/util/src/lib/auth/config.ts
@@ -2,8 +2,8 @@ import Keycloak from 'keycloak-js';
 
 export const keycloak = Keycloak({
   clientId: 'login-app',
-  realm: 'shellRealm',
+  realm: 'shell',
   // realm: 'shellRealm',
   // url: 'https://shell.vakt.com:9443/auth'
-  url: 'https://shell.vakt.com:9443/auth'
+  url: 'https://shell.qa03.login.dev02.vtest.info/auth'
 });
